fdaf137a3b3aae9bedab63200c0b9c8f75448c28
nematu <nayaz-akhundzada@mail.greenriver.edu>
2021-02-09 22:54:12 -0500

    updated data.layer

---
+++ model/data_layer.php
@@ -0,0 +1,17 @@
+<?php
+// 328 / model/ data_layer
+// get fuits
+function getFruit()
+{
+    $fruits = array('apple','banana','orange','kiwi','rambutan');
+    return $fruits;
+}
+
+/* Returns an associative array of array of names and salaries
+* @return array
+ *
+ */
+function getSalary()
+{
+    return $salaries = array('Jessica'=>200000, 'Heather'=>250000, 'Alex'=>300000);
+}
